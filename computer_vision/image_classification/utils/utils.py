b2f640c2a13717712fdf0d23c084e2c15ea509d5
AIEngineerOnDemand <giopantana@gmail.com>
2024-01-31 13:19:41 -0500

    Refactor load_data function to include detailed documentation

--- computer_vision/image_classification/utils/utils.py
+++ computer_vision/image_classification/utils/utils.py
@@ -3,9 +3,25 @@ from torchvision import datasets, transforms
 
 def load_data():
     """
-    Load and normalize the CIFAR10 training and test datasets using torchvision
-    """
+    Load and normalize the CIFAR10 training and test datasets using torchvision.
 
+    This function performs the following steps:
+
+    1. Defines a transform that will be applied to the images in the dataset. This transform first converts the images to PyTorch tensors (with transforms.ToTensor()) and then normalizes the tensors with mean and standard deviation of 0.5 for each of the three color channels (with transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))).
+
+    2. Loads the CIFAR10 training and test datasets, applies the transform to them, and wraps them in a DataLoader. The DataLoader will provide batches of 4 images at a time, shuffling the training data but not the test data. It will use 2 worker processes to load the data in parallel.
+
+    3. Defines the class labels for the CIFAR10 dataset.
+
+    The function returns the training and test data loaders and the class labels. The returned trainloader and testloader objects can be iterated over to get batches of images and their corresponding labels. The classes list can be used to map the numeric labels to their corresponding class names.
+
+    Note: The batch size and number of worker processes are hard-coded. Depending on your specific use case and the resources available on your machine, you might want to make these parameters configurable.
+
+    Returns:
+        trainloader (torch.utils.data.DataLoader): The DataLoader for the training data.
+        testloader (torch.utils.data.DataLoader): The DataLoader for the test data.
+        classes (tuple of str): The class labels for the CIFAR10 dataset.
+    """
     # Define a transform to normalize the data
     transform = transforms.Compose(
         [transforms.ToTensor(),
