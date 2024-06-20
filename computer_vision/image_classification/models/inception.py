f5782fc1a2a9a697fd467f612378482f31d5ab21
AIEngineerOnDemand <giopantana@gmail.com>
2024-06-19 19:05:25 -0500

    feat: Update torchvision import in Inception model

--- computer_vision/image_classification/models/inception.py
+++ computer_vision/image_classification/models/inception.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
-from torchvision.models import inception_v3
+from torchvision import models
 from .base_model import BaseModel
 
 class Inception(BaseModel):
     def __init__(self, num_classes=10):
         super(Inception, self).__init__(num_classes)
-        self.model = inception_v3(pretrained=True)
+        self.model = models.inception_v3(pretrained=True)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
