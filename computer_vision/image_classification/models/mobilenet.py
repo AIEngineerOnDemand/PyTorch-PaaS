fdb2fa53d2a5b483606b7f68c03790b31bc256c0
AIEngineerOnDemand <giopantana@gmail.com>
2024-07-03 15:41:39 -0500

    refactor: Update MobileNet model to use updated API with weights

--- computer_vision/image_classification/models/mobilenet.py
+++ computer_vision/image_classification/models/mobilenet.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
-from torchvision.models import mobilenet_v2
+from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
 from .base_model import BaseModel
 
 class MobileNet(BaseModel):
     def __init__(self, num_classes=10):
         super(MobileNet, self).__init__(num_classes)
-        self.model = mobilenet_v2(pretrained=True)
+        # Use the updated API with weights
+        weights = MobileNet_V2_Weights.IMAGENET1K_V1  # or use MobileNet_V2_Weights.DEFAULT for the most up-to-date weights
+        self.model = mobilenet_v2(weights=weights)
         self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, num_classes)
 
     def forward(self, x):
