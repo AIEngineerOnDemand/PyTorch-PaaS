80d6221f0b0d5b29e43b7aa038fee929f11a991d
AIEngineerOnDemand <giopantana@gmail.com>
2024-07-03 15:41:20 -0500

    refactor: Update DenseNet model to use updated API with weights

--- computer_vision/image_classification/models/densenet.py
+++ computer_vision/image_classification/models/densenet.py
@@ -1,11 +1,13 @@
-from torchvision.models import densenet121
+from torchvision.models import densenet121, DenseNet121_Weights
 import torch.nn as nn
 from .base_model import BaseModel
 
 class DenseNet(BaseModel):
     def __init__(self, num_classes=10):
         super(DenseNet, self).__init__(num_classes)
-        self.model = densenet121(pretrained=True)
+        # Use the updated API with weights
+        weights = DenseNet121_Weights.IMAGENET1K_V1  # or use DenseNet121_Weights.DEFAULT for the most up-to-date weights
+        self.model = densenet121(weights=weights)
         num_features = self.model.classifier.in_features
         self.model.classifier = nn.Linear(num_features, self.num_classes)
         
